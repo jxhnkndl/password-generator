da2dbb2b89ea5ed3a575d1949d1a9441c7d01cad
jxhnkndl <jkroyston@gmail.com>
2020-12-05 15:34:07 -0500

    Create functional arrays of values for iteration purposes

--- script.js
+++ script.js
@@ -8,8 +8,13 @@ var lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 
 var numArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-var special = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "\/", "]", "^", "_", "`", "{", "|", "}", "~"];
+var specialArr = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "\/", "]", "^", "_", "`", "{", "|", "}", "~"];
 
+// Array of character arrays for iteration methods
+var charArrays = [upperArr, lowerArr, numArr, spceialArr];
+
+// Final Password Generation Array
+var passArr = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -20,6 +25,7 @@ function writePassword() {
 
 }
 
+
 // Generate password based on user inputs
 function generatePassword() {
  
@@ -29,16 +35,27 @@ function generatePassword() {
   // Initiate the password generation sequence
   alert('The following questions will ask you about your preferences before creating you a new, secure password.')
 
-  // Capture user's desired password length from prompt
-  passwordLength = prompt('Please choose a number between 8 and 128 characters for your password.');
+  // Validate accurate password length between 8 and 128 characters
+  while (passwordLength < 8 || passwordLength > 128) {
+    // Capture user password length as string
+    var passwordInput = prompt('Please choose a number between 8 and 128 characters for your password.');
+
+    // Convert numeric string data into number data
+    passwordLength = parseInt(passwordInput);
+  }
 
   // Determine which type of characters the user intends to include
   var uppercase = confirm('Would you like to include uppercase letters?');
   var lowercase = confirm('Would you like to include lowercase letters?');
-  var numbers = confrim('Would you like to include numbers?');
+  var numbers = confirm('Would you like to include numbers?');
   var special = confirm('Would you like to include special characters?');
 
+  // User input array
+  var userChoices = [uppercase, lowercase, numbers, special];
 
+
+
+  console.log(charArr);
 }
 
 // Add event listener to generate button
