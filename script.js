256019705f57ac9976ff4a1155c19ef6339eeb21
jxhnkndl <jkroyston@gmail.com>
2020-12-04 19:05:25 -0500

    Create character arrays to construct random password from

--- script.js
+++ script.js
@@ -3,12 +3,29 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // Create password in generatePassword() and save it
   var password = generatePassword();
+
+  // Capture the UI field to display the password
   var passwordText = document.querySelector("#password");
 
+  // Paint the UI field with the newly generated password
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generate Password
+function generatePassword() {
+
+  // Alphanumiric characters
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  // Special non-alphanumeric characters
+  var specialChars = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+  // Concatenate alphanumeric + special character arrays
+  var passwordArr = chars.split('').concat(specialChars);
+
+}
