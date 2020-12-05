9452ec8518dc4faeba02e69966e1cae9b853650b
jxhnkndl <jkroyston@gmail.com>
2020-12-05 17:23:47 -0500

    Write passwordLength function to capture and validate user password length

--- script.js
+++ script.js
@@ -12,6 +12,47 @@ var numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialArr = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "\/", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 
+// Capture and validate user's desired password length
+function passwordLength() {
+
+  // Init length variable for validation
+  var length = 0;
+
+  // Basic regex to validate that password length is only digits
+  var alphabet = /[a-zA-Z]/g
+
+  // Validate password length meets criteria
+  while (length < 8 || length > 128) {
+
+    // Capture user password length as string
+    length = prompt('Please choose a number between 8 and 128 characters for your password.');
+
+    // Validate no alphabetic characters in password length
+    if (alphabet.test(length)) {
+      alert('The password length may only contain numbers.');
+
+      // Reset length to restart the loop
+      length = 0;
+    }
+  }
+
+  // Convert string number into actual number
+  return parseInt(length);
+}
+
+
+// Generate Password
+function generatePassword() {
+
+  // Initiate the password generation sequence
+  alert('The following questions will ask you about your preferences before creating you a new, secure password.')
+
+  // Capture user's desired password length
+  var length = passwordLength();
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -20,3 +61,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
