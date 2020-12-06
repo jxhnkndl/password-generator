c724a874d4dc1d67f1f00fa17e7911ab971d00bf
jxhnkndl <jkroyston@gmail.com>
2020-12-06 16:31:20 -0500

    Refactor code blocks and comments

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// PASSWORD GENERATOR
+// J.K. Royston
+
 // UI Vars
 var generateBtn = document.querySelector("#generate");
 
-
-// Individual Character Arrays
+// Character Arrays
 var upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 var lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -11,78 +13,66 @@ var numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 var specialArr = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "\/", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-// Summed Character Arrays
 var characters = [upperArr, lowerArr, numArr, specialArr];
 
 
 // Capture and validate user's desired password length
 function passwordLength() {
-
-  // Init length variable for validation
   var length = 0;
-
-  // Basic regex to validate that password length is only digits
+  // Basic alphabetic regex pattern to validate that length input only contains digits
   var alphabet = /[a-zA-Z]/g
 
-  // Validate password length meets criteria
+  // Validate password length meets min/max criteria
   while (length < 8 || length > 128) {
-
-    // Capture user password length as string
     length = prompt('Please choose a number between 8 and 128 characters for your password.');
 
-    // Validate no alphabetic characters in password length
     if (alphabet.test(length)) {
-      alert('The password length may only contain numbers.');
-
-      // Reset length to restart the loop
+      alert('The password length may only contain numbers. Please try again.');
       length = 0;
     }
   }
 
-  // Convert string number into actual number
-  return parseInt(length);
+  return length;
 }
 
-// Capture user character choices
+// CURRENTLY WORKING
 function charChoices() {
 
-  // Determine which type of characters the user intends to include
-  
+  // Capture which type of characters the user wants to include (needs to run at least once)
   do {
-    alert('Please select at least one character type to construct your password.');
+    alert('Please select at least one character type to include in your password.');
 
-    // Capture user's character preferences
     var uppercase = confirm('Would you like to include uppercase letters?');
     var lowercase = confirm('Would you like to include lowercase letters?');
     var numbers = confirm('Would you like to include numbers?');
     var special = confirm('Would you like to include special characters?');
   
-    // Continue this loop as long as the user has not selected at least one character type
+    // Loop back if the user has not confirmed at least one character type
   } while ((!uppercase && !lowercase && !numbers && !special))
 
-  // Init user choice confirmed password arry
+  // Init password array that will contain only character types chosen by user
   var passArr;
 
-  // Check for uppercase and add to password character array
+  // Check Password Array and Uppercase
   if (uppercase) {
     passArr = upperArr;
   }
 
-  // Check for lowercase and add to previous character choices
+  // Check Password Array and Lowercase
   if (!passArr && lowercase) {
     passArr = lowerArr;
   } else if (passArr && lowercase) {
     passArr = passArr.concat(lowerArr);
   } 
 
-  // Check for numbers and add to previous character choices
+  // Check Password Array and Numbers
   if (!passArr && numbers) {
     passArr = numArr;
   } else if (passArr && numbers) {
     passArr = passArr.concat(numArr);
   } 
 
-  // Check for special character and add to previous character choices
+  // Check Password Array and Specials
   if (!passArr && special) {
     passArr = specialArr;
   } else if (passArr && special) {
@@ -97,7 +87,7 @@ function randomNumber(length) {
   return Math.floor(Math.random() * length);
 }
 
-// Generate password (primary application logic)
+// Generate password
 function generatePassword() {
 
   // Init final password variable
@@ -106,17 +96,13 @@ function generatePassword() {
   // Initiate the password generation sequence
   alert('The following questions will ask you about your preferences before creating you a new, secure password.')
 
-  // Capture user's desired password length
+  // Capture and store user's preferences
   var length = passwordLength();
-
-  // Capture array containing only characters user chose
   var passwordArr = charChoices();
 
-  // Loop through the number of characters chosen by the user
+  // Loop through the chosen password length and apply a random character to each index
   for (var i = 0; i < length; i++) {
-    // Generate a random number to pull a character from the password array
     var randomIndex = randomNumber(passwordArr.length);
-    // Concatenate the random character onto the final password
     password += passwordArr[randomIndex];
   }
 
