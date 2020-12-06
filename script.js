e8bc31438db40cd231adbf40829ce39eb2c9b009
jxhnkndl <jkroyston@gmail.com>
2020-12-06 17:47:12 -0500

    Refactor data storage types and simplify functions

--- script.js
+++ script.js
@@ -4,18 +4,6 @@
 // UI Vars
 var generateBtn = document.querySelector("#generate");
 
-// Character Arrays
-var upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-var lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-var numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-var specialArr = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "\/", "]", "^", "_", "`", "{", "|", "}", "~"];
-
-var characters = [upperArr, lowerArr, numArr, specialArr];
-
-
 // Capture and validate user's desired password length
 function passwordLength() {
   var length = 0;
@@ -26,6 +14,7 @@ function passwordLength() {
   while (length < 8 || length > 128) {
     length = prompt('Please choose a number between 8 and 128 characters for your password.');
 
+    // Validate password length does not contain any non-numeric characters
     if (alphabet.test(length)) {
       alert('The password length may only contain numbers. Please try again.');
       length = 0;
@@ -35,59 +24,70 @@ function passwordLength() {
   return length;
 }
 
-// CURRENTLY WORKING
+// Get user character choices
 function charChoices() {
 
   // Capture which type of characters the user wants to include (needs to run at least once)
   do {
     alert('Please select at least one character type to include in your password.');
 
-    var uppercase = confirm('Would you like to include uppercase letters?');
-    var lowercase = confirm('Would you like to include lowercase letters?');
-    var numbers = confirm('Would you like to include numbers?');
-    var special = confirm('Would you like to include special characters?');
+    var charUpper = confirm('Would you like to include uppercase letters?');
+    var charLower = confirm('Would you like to include lowercase letters?');
+    var charNum = confirm('Would you like to include numbers?');
+    var charSpecial = confirm('Would you like to include special characters?');
   
     // Loop back if the user has not confirmed at least one character type
-  } while ((!uppercase && !lowercase && !numbers && !special))
+  } while ((!charUpper && !charLower && !charNum && !charSpecial))
 
-  // Init password array that will contain only character types chosen by user
-  var passArr;
-
-  // Check Password Array and Uppercase
-  if (uppercase) {
-    passArr = upperArr;
+  // Build character objects with use preferences and character arrays
+  var uppercase = {
+    array: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    include: charUpper
   }
 
-  // Check Password Array and Lowercase
-  if (!passArr && lowercase) {
-    passArr = lowerArr;
-  } else if (passArr && lowercase) {
-    passArr = passArr.concat(lowerArr);
-  } 
+  var lowercase = {
+    array: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    include: charLower
+  }
 
-  // Check Password Array and Numbers
-  if (!passArr && numbers) {
-    passArr = numArr;
-  } else if (passArr && numbers) {
-    passArr = passArr.concat(numArr);
-  } 
+  var numbers = {
+    array: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    include: charNum
+  }
 
-  // Check Password Array and Specials
-  if (!passArr && special) {
-    passArr = specialArr;
-  } else if (passArr && special) {
-    passArr = passArr.concat(specialArr);
-  } 
+  var special = {
+    array: ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "\/", "]", "^", "_", "`", "{", "|", "}", "~"],
+    include: charSpecial
+  }
+
+  // Create character array to iterate through
+  var characters = [uppercase, lowercase, numbers, special];
+
+  // Init array that will contain only character types confirmed by user
+  var passArr;
+
+  // Loop through each character type and either include or exclude it from final array
+  characters.forEach(type => {
+    if (!passArr && type.include) {
+      passArr = type.array;
+      console.log(passArr);
+
+    } else if (passArr && type.include) {
+      passArr = passArr.concat(type.array);
+      console.log(passArr);
+    }
+  });
 
   return passArr;
 }
+
 
 // Generate random number within range of 0 and length
 function randomNumber(length) {
   return Math.floor(Math.random() * length);
 }
 
-// Generate password
+// Generate password (primary application logic)
 function generatePassword() {
 
   // Init final password variable
@@ -96,11 +96,11 @@ function generatePassword() {
   // Initiate the password generation sequence
   alert('The following questions will ask you about your preferences before creating you a new, secure password.')
 
-  // Capture and store user's preferences
+  // Capture user's desired length and character criteria
   var length = passwordLength();
   var passwordArr = charChoices();
 
-  // Loop through the chosen password length and apply a random character to each index
+  // Loop through the chosen length and apply a random character to each index
   for (var i = 0; i < length; i++) {
     var randomIndex = randomNumber(passwordArr.length);
     password += passwordArr[randomIndex];
@@ -114,6 +114,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // Paint UI with final unique password
   passwordText.value = password;
   console.log(password);
 }
